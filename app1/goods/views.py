83e8c57d21624b6596c94a0c87d18a071208cca2
kathlleen <katrina2005by@gmail.com>
2024-08-15 15:05:38 +0300

    Tag for - endfor for goods

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -5,7 +5,58 @@ def catalog(request):
     context = {
         "title" : "SQ R3 - Catalog",
         "page_title" : "Catalog",
-        "page_description" : "Find out some information about us"
+        "page_description" : "Find out some information about us",
+        "goods" : [
+            {
+                "title" : "Furniture name",
+                "price" : "$250",
+                "description" : "It is a long established fact that a reader will be the service.",
+                "image" : "img/f1.jfif"
+            },
+            {
+                "title": "Furniture name",
+                "price": "$250",
+                "description": "It is a long established fact that a reader will be the service.",
+                "image": "img/f2.jfif"
+            },
+            {
+                "title" : "Furniture name",
+                "price" : "$250",
+                "description" : "It is a long established fact that a reader will be the service.",
+                "image" : "img/f3.jfif"
+            },
+            {
+                "title" : "Furniture name",
+                "price" : "$250",
+                "description" : "It is a long established fact that a reader will be the service.",
+                "image" : "img/f4.jfif"
+            },
+            {
+                "title": "Furniture name",
+                "price": "$250",
+                "description": "It is a long established fact that a reader will be the service.",
+                "image": "img/f1.jfif"
+            },
+            {
+                "title": "Furniture name",
+                "price": "$250",
+                "description": "It is a long established fact that a reader will be the service.",
+                "image": "img/f2.jfif"
+            },
+            {
+                "title": "Furniture name",
+                "price": "$250",
+                "description": "It is a long established fact that a reader will be the service.",
+                "image": "img/f3.jfif"
+            },
+            {
+                "title": "Furniture name",
+                "price": "$250",
+                "description": "It is a long established fact that a reader will be the service.",
+                "image": "img/f4.jfif"
+            }
+        ]
+
     }
     return render(request, 'goods/catalog.html', context)
 
