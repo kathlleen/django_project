43249bb12272ad9151538f7bad1fdf4446b269f5
kathlleen <katrina2005by@gmail.com>
2024-08-20 15:25:17 +0300

    Slug converter for categories

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render
 from goods.models import Products
-def catalog(request):
+def catalog(request, category_slug):
     # categories = Categories.objects.all()
-    goods = Products.objects.all()
+
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+
+    paginator = Raginator()
 
     context = {
         "title" : "SQ R3 - Catalog",
         "page_title" : "Catalog",
-        "page_description" : "Find out some information about us",
+        "page_description" : "Order best furniture right now",
         # 'categories': categories,
         'goods' : goods,
     }
+
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
+def product(request, product_slug):
 
+    product_val = Products.objects.get(slug=product_slug)
     context = {
-        "title" : "SQ R3 - Product name",
-        "page_title" : "Product name",
-        "page_description" : "Find out some information about us"
+        'product':product_val
     }
-    return render(request, 'goods/product.html', context)
+    return render(request, 'goods/product.html', context=context)
+
+# def product(request):
+#
+#     context = {
+#         "title" : "SQ R3 - Product name",
+#         "page_title" : "Product name",
+#         "page_description" : "Find out some information about us"
+#     }
+#     return render(request, 'goods/product.html', context)
