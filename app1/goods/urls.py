ddb97a74be185dc583fccf8cee8ab7242a2a765c
kathlleen <katrina2005by@gmail.com>
2024-08-26 01:12:12 +0300

    Search by keywords

--- app1/goods/urls.py
+++ app1/goods/urls.py
@@ -6,6 +6,8 @@ app_name = "goods"
 
 urlpatterns = [
     # path('', views.catalog, name='index'),
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('product/<slug:product_slug>/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product')
+
 ]
