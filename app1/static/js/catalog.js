6f63b20f6d93baee803135200397d0bfd264107f
kathlleen <katrina2005by@gmail.com>
2024-09-05 12:09:28 +0300

    Carts templates are ready

--- app1/static/js/catalog.js
+++ app1/static/js/catalog.js
@@ -9,3 +9,26 @@ document.addEventListener('click', function(event) {
         document.querySelector('.dropdown-content').classList.remove('show');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+        const cartBtn = document.getElementById('cart-btn');
+        const cartModal = document.getElementById('cartModal');
+        const closeModal = document.getElementById('closeModal');
+
+        // Открытие модального окна при клике на кнопку корзины
+        cartBtn.addEventListener('click', function() {
+            cartModal.style.display = 'block';
+        });
+
+        // Закрытие модального окна при клике на кнопку закрытия
+        closeModal.addEventListener('click', function() {
+            cartModal.style.display = 'none';
+        });
+
+        // Закрытие модального окна при клике вне области модального окна
+        window.addEventListener('click', function(event) {
+            if (event.target === cartModal) {
+                cartModal.style.display = 'none';
+            }
+        });
+    });
